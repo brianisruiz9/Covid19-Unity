3bd66157b0beeadda769bc2f1e05485a3f21e5e6
brianisruiz9 <brianisp@hotmail.com>
2020-05-17 14:07:07 -0500

    Navegación en el personaje

---
+++ Assets/AgentController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentController : MonoBehaviour
+{
+    NavMeshAgent myNavMeshAgent;
+    public Transform target;
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        myNavMeshAgent.SetDestination(target.position);
+    }
+}
